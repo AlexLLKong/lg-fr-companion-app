e004c18015dc1bb69883ec4fd322566feb10b45b
AlexLLKong <alexkonghockey@gmail.com>
2020-11-23 06:51:45 -0800

    Add react-router
    [Ticket: X]

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,47 @@
-import { Button } from 'reactstrap'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 import './App.css'
 
 function App() {
 	return (
-		<div className="App">
-			<Button>Hi</Button>
-		</div>
+		<Router>
+			<nav>
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/about">About</Link>
+					</li>
+					<li>
+						<Link to="/users">Users</Link>
+					</li>
+				</ul>
+			</nav>
+			<Switch>
+				<Route path="/about">
+					<About />
+				</Route>
+				<Route path="/users">
+					<Users />
+				</Route>
+				<Route path="/">
+					<Home />
+				</Route>
+			</Switch>
+		</Router>
 	)
+}
+
+function Home() {
+	return <h2>Home</h2>
+}
+
+function About() {
+	return <h2>About</h2>
+}
+
+function Users() {
+	return <h2>Users</h2>
 }
 
 export default App
